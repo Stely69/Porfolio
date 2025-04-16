ca412528a698cec164422383811d55f00d9c1b54
stely69 <you@example.com>
2025-04-16 16:28:31 -0500

    Add proyectos data file to define asset management project details

---
+++ src/data/proyecto.ts
@@ -0,0 +1,13 @@
+import foto from "../assets/images/GEDAC.png";
+
+export const proyectos = [
+  {
+    titulo: " Sistema de Gestión de Activos",
+    descripcion: "Una página web para gestionar activos de una empresa.",
+    imagen: foto,
+    tecnologias: ["PHP", "TailwindCSS", "HTML"],
+    demo: "#",
+    codigo: "https://github.com/Stely69/mesa-de-ayuda"
+  },
+
+];
