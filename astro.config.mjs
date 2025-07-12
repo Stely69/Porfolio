502933a47c8306118a2966b2f4a120c437dd936e
Steven Vanegas Gomez <128706059+Stely69@users.noreply.github.com>
2025-07-12 20:34:42 +0000

    chore: update dependencies and modify wrangler configuration
    - Added @astrojs/cloudflare dependency to package.json
    - Removed duplicate @astrojs/tailwind dependency from package.json
    - Updated wrangler.toml to use pages_build_output_dir instead of assets directory

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   integrations: [
     tailwind(),
     icon(),
   ],
+
+  adapter: cloudflare(),
 });
