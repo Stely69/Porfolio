fda15b376852d055ec8010f44837343d364cc1da
Steven Vanegas Gomez <williamsteven237g@gmail.com>
2025-10-16 14:24:52 -0500

    feat: actualizar imágenes de proyectos y agregar nueva entrada para Ecommerce Distribuidora

--- src/data/proyecto.ts
+++ src/data/proyecto.ts
@@ -1,13 +1,21 @@
-import foto from "../assets/images/GEDAC.png";
+import GEDAC from "@/assets/images/GEDAC.png";
+import Distribuidora from "@/assets/images/Distribuidora.png";
+
 
 export const proyectos = [
   {
     titulo: " Sistema de Gestión de Activos",
     descripcion: "Una página web para gestionar activos de una empresa.",
-    imagen: foto,
+    imagen: GEDAC,
     tecnologias: ["PHP", "TailwindCSS", "HTML"],
     demo: "#",
     codigo: "https://github.com/Stely69/mesa-de-ayuda"
   },
-
+  {
+    titulo: "Ecommerce Distribuidora",
+    descripcion: "Una plataforma de comercio electrónico para una distribuidora.",
+    imagen: Distribuidora,
+    tecnologias: ["React", "Hono", "TailwindCSS", "TypeScript", "SQLite"],
+    demo: "https://damigofeng.dogalyir.workers.dev",
+  },
 ];
